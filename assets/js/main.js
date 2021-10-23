87e97a4bd6fac222ee19987c1281ec1dd033d11b
Shashwat Singh Raghav <54545252+ShashwatSinghRaghav@users.noreply.github.com>
2021-10-23 13:51:37 +0530

    casestudy added

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,7 +62,8 @@ sr.reveal('.home__img',{delay: 400});
 sr.reveal('.home__social-icon',{ interval: 200}); 
 
 /*SCROLL ABOUT*/
-sr.reveal('.about__img',{}); 
+sr.reveal('.about__img',{});
+sr.reveal('.casestudy__container',{delay: 400}); 
 sr.reveal('.about__subtitle',{delay: 400});
 sr.reveal('.about__subtitle1',{delay: 400}); 
 sr.reveal('.about__text',{delay: 400});
